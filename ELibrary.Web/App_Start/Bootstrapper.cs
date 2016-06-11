bf1accb212529428a10596b6443f2a29f8e07ce7
jacwu <wsjeep29@hotmail.com>
2016-06-11 23:56:42 +0800

    Step10 - Configure Autofac

---
+++ ELibrary.Web/App_Start/Bootstrapper.cs
@@ -0,0 +1,46 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using AutoMapper;
+using ELibrary.Data;
+using ELibrary.Data.Infra;
+using ELibrary.Data.Repositories;
+using ELibrary.Model.Entities;
+using ELibrary.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ELibrary.Web
+{
+    public static class Bootstrapper
+    {
+        public static void Run()
+        {
+            ConfigureAutofacContainer();
+        }
+
+        private static void ConfigureAutofacContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(BookRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(BookService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
