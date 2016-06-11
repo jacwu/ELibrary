82ca2c6bb3657ca83bd7cc2cd1f8b0965dc3c409
jacwu <wsjeep29@hotmail.com>
2016-06-11 21:54:59 +0800

    Step2 - Add Content and fonts
    Add content, fonts, scripts and css; add bundleconfig.cs, configure
    bundle in global.asax

---
+++ ELibrary.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ELibrary.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/jquery.js",
+                        "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                        "~/css/*.css"));
+        }
+    }
+}
--- ELibrary.Web/Global.asax.cs
+++ ELibrary.Web/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ELibrary.Web
@@ -13,6 +14,7 @@ namespace ELibrary.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
