8e86fc43d1dd9564edffc78f5dd121fa05903f85
jacwu <wsjeep29@hotmail.com>
2016-06-11 23:17:05 +0800

    Step7 - Enable Migration
    Add ElibraryEntities.cs

---
+++ ELibrary.Data/Migrations/201606111510588_Initial.cs
@@ -0,0 +1,76 @@
+namespace ELibrary.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false),
+                        AuthorName = c.String(),
+                        Description = c.String(nullable: false),
+                        PublishYear = c.Int(nullable: false),
+                        ImageName = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        OpenDate = c.DateTime(nullable: false),
+                        CloseDate = c.DateTime(),
+                        UserName = c.String(nullable: false),
+                        Book_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Books", t => t.Book_Id)
+                .Index(t => t.Book_Id);
+            
+            CreateTable(
+                "dbo.Tags",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        ImageName = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.TagBooks",
+                c => new
+                    {
+                        Tag_Id = c.Int(nullable: false),
+                        Book_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Tag_Id, t.Book_Id })
+                .ForeignKey("dbo.Tags", t => t.Tag_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Books", t => t.Book_Id, cascadeDelete: true)
+                .Index(t => t.Tag_Id)
+                .Index(t => t.Book_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TagBooks", "Book_Id", "dbo.Books");
+            DropForeignKey("dbo.TagBooks", "Tag_Id", "dbo.Tags");
+            DropForeignKey("dbo.Orders", "Book_Id", "dbo.Books");
+            DropIndex("dbo.TagBooks", new[] { "Book_Id" });
+            DropIndex("dbo.TagBooks", new[] { "Tag_Id" });
+            DropIndex("dbo.Orders", new[] { "Book_Id" });
+            DropTable("dbo.TagBooks");
+            DropTable("dbo.Tags");
+            DropTable("dbo.Orders");
+            DropTable("dbo.Books");
+        }
+    }
+}
