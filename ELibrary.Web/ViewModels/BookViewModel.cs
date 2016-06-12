ad9e3e0fe5c83da76732dd096f1770023567fffa
jacwu <wsjeep29@hotmail.com>
2016-06-12 12:18:48 +0800

    Step17 - Add Form Validation

--- ELibrary.Web/ViewModels/BookViewModel.cs
+++ ELibrary.Web/ViewModels/BookViewModel.cs
@@ -10,14 +10,26 @@ namespace ELibrary.Web.ViewModels
 {
     public class BookViewModel
     {
+        [Required]
         public string Title { get; set; }
+
         [Display(Name = "Author Name")]
         public string AuthorName { get; set; }
+
+        [Required]
+        [StringLength(300, MinimumLength = 5)]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
+        [Required]
         [Display(Name = "Publish Year")]
         public int PublishYear { get; set; }
+
         [Display(Name = "Cover")]
+        [Required]
         public HttpPostedFileBase CoverImg { get; set; }
+
+        [Required]
         [Display(Name = "Category")]
         public IEnumerable<int> TagIds { get; set; }
     }
