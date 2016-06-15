4a251f627cedba4761f42df2f6dedee3bf7a6846
xinjinmsn <xinjin@msn.com>
2016-06-15 21:39:07 +0800

    Your First Web API Controller

---
+++ ELibrary.API/Controllers/TagsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ELibrary.API.Controllers
+{
+    [Route("api/library/tags/{tagid?}", Name = "Tags")]
+    public class TagsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
---
+++ ELibrary.API/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(ELibrary.API.Startup))]
+
+namespace ELibrary.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            app.UseWebApi(config);
+        }
+    }
+}
