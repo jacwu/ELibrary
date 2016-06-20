d3506ae4321cf0922b42f67b43cca8c00a274fe7
xinjinmsn <xinjin@msn.com>
2016-06-20 15:17:18 +0800

    Isolated Scope

--- ELibrary.Web/Scripts/app/directives/bookinfocard.js
+++ ELibrary.Web/Scripts/app/directives/bookinfocard.js
@@ -3,6 +3,9 @@
 
     angular.module("elibrary.web").directive("bookInfoCard", function () {
         return {
+            scope: {
+                book: "="
+            },
             templateUrl: "Scripts/app/directives/bookinfocard.html",
             controller: ["$scope", BookInfoCardController]
         };
