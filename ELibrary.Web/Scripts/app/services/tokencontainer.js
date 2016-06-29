1e6be4e8c8f6e8e43a3af5d52c1afdcb4e7e8df8
xinjinmsn <xinjin@msn.com>
2016-06-29 16:34:46 +0800

    Use Access Token in AngularJS

---
+++ ELibrary.Web/Scripts/app/services/tokencontainer.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+    angular.module("elibrary.web").factory("tokenContainer",
+        ["$location", "$cookies", function ($location, $cookies) {
+
+            return {
+                getToken: function () {
+                    var access_token = $cookies.get('access_token');
+                   
+                    return access_token;
+                }
+            };
+
+
+        }]);
+
+
+}());
